5f495ac1cad695054359314b63e24fe7bb445630
daiyizheng <387942239@qq.com>
2024-06-23 22:08:07 +0800

    faiss

--- 10-langchain/neo4j-cypher/ingest.py
+++ 10-langchain/neo4j-cypher/ingest.py
@@ -7,15 +7,17 @@ Author           :daiyizheng
 Email            :387942239@qq.com
 Version          :1.0
 '''
-
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 from langchain_community.graphs import Neo4jGraph
 
-graph = Neo4jGraph(url="bolt://192.168.26.200:7687", 
-                   username="neo4j", 
-                   password="zju20230808")
+graph = Neo4jGraph(url=os.environ["ENO4J_URL"], 
+                   username=os.environ["ENO4J_USER"], 
+                   password=os.environ["EN04J_PASSWORD"])
 
 res = graph.query(
-    """
+"""
 MERGE (m:Movie {name:"Top Gun"})
 WITH m
 UNWIND ["Tom Cruise", "Val Kilmer", "Anthony Edwards", "Meg Ryan"] AS actor
